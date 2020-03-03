091cc8fad6983c26ca48d8376104b2d85e81e2e1
Soaple <inje@soaple.io>
2020-03-03 23:31:13 +0900

    Preview image added.

--- stickyboard.config.js
+++ stickyboard.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     title: 'CoronaBoard',
     description: 'Global Corona Dashboard powered by StickyBoard',
-    meta_image: '/static/image/CoronaBoard_logo.png',
+    meta_image: '/static/image/CoronaBoard_preview.png',
     favicon: '/static/image/favicon.png',
 };
